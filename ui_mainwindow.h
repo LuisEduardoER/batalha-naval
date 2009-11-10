c4ba05d50a724b3cff7ee9f7e47cd885a46ad5ae
andrezamazaki <andrezamazaki@yahoo.com.br>
2009-11-10 23:00:03 +0000

    trocado QFrame pelo QPushButton

--- ui_mainwindow.h
+++ ui_mainwindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading ui file 'mainwindow.ui'
 **
-** Created: Tue 10. Nov 13:12:58 2009
+** Created: Tue 10. Nov 20:55:45 2009
 **      by: Qt User Interface Compiler version 4.5.1
 **
 ** WARNING! All changes made in this file will be lost when recompiling ui file!
